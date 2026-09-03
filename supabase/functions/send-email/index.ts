ebfb73f6ee95fff4b7feab039d908b21f2fcc3d2
Rooster <david.flinton@gmail.com>
2026-09-03 06:18:35 -0500

    Quick update

---
+++ supabase/functions/send-email/index.ts
@@ -0,0 +1,42 @@
+const corsHeaders = {
+  'Access-Control-Allow-Origin': '*',
+  'Access-Control-Allow-Headers': 'authorization, x-client-info, apikey, content-type',
+};
+
+Deno.serve(async (req) => {
+  // Handle CORS preflight requests
+  if (req.method === 'OPTIONS') {
+    return new Response('ok', { headers: corsHeaders });
+  }
+  
+  try {
+    const RESEND_API_KEY = Deno.env.get('RESEND_API_KEY');
+    const { to, subject, html } = await req.json();
+
+    const res = await fetch('https://api.resend.com/emails', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${RESEND_API_KEY}`,
+      },
+      body: JSON.stringify({
+        from: 'Trailhead Admin <onboarding@resend.dev>', // Change this once you verify your custom domain
+        to: [to],
+        subject: subject,
+        html: html,
+      }),
+    });
+
+    const data = await res.json();
+    
+    return new Response(JSON.stringify(data), {
+      headers: { ...corsHeaders, 'Content-Type': 'application/json' },
+      status: 200,
+    });
+  } catch (error) {
+    return new Response(JSON.stringify({ error: error.message }), {
+      headers: { ...corsHeaders, 'Content-Type': 'application/json' },
+      status: 400,
+    });
+  }
+});
